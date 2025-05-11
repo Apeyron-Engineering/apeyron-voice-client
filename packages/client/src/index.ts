b3e2e4951ca15c516825ce94b8535121624156ed
Gabriele <gabriele.notonica17@gmail.com>
2025-05-11 18:52:39 +0200

    Increment version to 1.4.3 for voice-client and 2.3.3 for react-voice-client; lower audio input threshold for improved sound detection.

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -413,7 +413,7 @@ export class Conversation {
   private onInputWorkletMessage = (event: MessageEvent): void => {
     const rawAudioPcmData = event.data[0];
     const maxVolume = event.data[1];
-    const threshold = 0.0015;
+    const threshold = 0.0005;
 
     console.log("maxVolume", maxVolume);
     console.log("threshold", threshold);
