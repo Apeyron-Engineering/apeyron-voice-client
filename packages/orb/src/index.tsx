27a2bec03db99c9823df203f6f7c71b3614a3e4b
Gabriele N <gabriele.notonica17@gmail.com>
2025-06-20 13:01:11 +0200

    added tailwind

--- packages/orb/src/index.tsx
+++ packages/orb/src/index.tsx
@@ -1,2 +1,4 @@
+import './globals.css';
+
 export { ConversationProvider, useConversationContext } from './providers/ConversationProvider';
 export { default as OrbFrame } from './components/OrbFrame';
